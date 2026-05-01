7097eb0904250bbc301e5cf208567ff1dec1b850
nimratariq936 <nimratariq936@gmail.com>
2026-05-01 15:10:30 +0500

    Update Firebase auth initialization for platform support
    Refactor Firebase authentication initialization for web and React Native platforms.

--- firebase.js
+++ firebase.js
@@ -1,8 +1,9 @@
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from 'firebase/app';
-import { getReactNativePersistence, initializeAuth } from 'firebase/auth'; // Using correct React Native initialization
+import { getAuth, getReactNativePersistence, initializeAuth } from 'firebase/auth'; 
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { Platform } from 'react-native';
 
 const firebaseConfig = {
   apiKey: "AIzaSyD3BA0gVh8OwxqghmCKlR00m_oq3aC4Ha8",
@@ -15,8 +16,17 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = initializeAuth(app);
+let auth;
+if (Platform.OS === 'web') {
+  auth = getAuth(app);
+} else {
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+  });
+}
+
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 
+export { auth };
 export default app;
